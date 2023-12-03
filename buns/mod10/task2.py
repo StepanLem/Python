9ca0f604d5c6c96e7491d33238b470994ba2a458
StepanLem <128487145+StepanLem@users.noreply.github.com>
2023-12-03 13:58:02 +0500

    Update task2.py

--- buns/mod10/task2.py
+++ buns/mod10/task2.py
@@ -4,7 +4,7 @@ import re
 
 def get_h3_headings(url):
     response = requests.get(url)
-    headings = re.findall(r'<h3.*?>(.*?)</h3>', response.text, re.DOTALL)
+    headings = re.findall(r'<h3.*?>(.*?)</h3>', response.text)
     return headings
 
 
